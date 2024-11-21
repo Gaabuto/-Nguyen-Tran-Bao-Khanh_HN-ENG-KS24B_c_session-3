19a65f3ff30e30c0abbfe118483449d87013ed24
Gaabuto <khanh040604@gmail.com>
2024-11-21 20:33:23 +0700

    3.6.cpp

--- 3.6.cpp
+++ 3.6.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main(){
-	int so;
-	printf("moi ban nhap so bat ky co 4 chu so");
-	scanf("%d", &so);
-	
+	float b;
+	printf("moi ban nhap do dai canh cua tam giac ");
+	scanf("%f", &b);
+	float h;
+	printf("moi ban nhap chieu cao cua tam giac ");
+	scanf("%f", &h);
+	float dienTich = ( b * h ) / 2;
+	printf ("dien tich cua hinh tam giac co dien tich la %.2f", dienTich);
 	return 0;
 }
